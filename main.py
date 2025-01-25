5b9a43faca3bfccaeb5705f957a2383a0703b3db
ridhorionaldo <ilahirido505@gmail.com>
2025-01-25 23:27:16 +0700

    Changes to be committed: 	modified:   main.py 	new file:   static/css/adminlte.css 	new file:   static/css/style.css 	renamed:    templates/index.html -> templates/home.html 	new file:   templates/login.html

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template("index.html")
+    return render_template("login.html")
+
+@app.route('/home')
+def home():
+    return render_template("home.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
